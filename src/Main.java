471ce74aa9ea18adcf6ce88daef4ffdea6000b62
Niroshan Sandeepa <niroshans@rcstotech.com>
2020-06-14 16:34:39 +0530

    Add hi(String name) to Main.java & call that method inside main method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        hi("Niroshan");
+    }
+
+    private static void hi(String name) {
+        System.out.println("Hi, " + name);
     }
 }
